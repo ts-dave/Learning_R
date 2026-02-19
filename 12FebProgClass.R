9bf949150acbf407a0e871a7f1ea6bd47131cb3c
Tsatsu Dave <tsatsu.code@gmail.com>
2026-02-19 10:15:23 +0000

    19th Feb programming class

--- 12FebProgClass.R
+++ 12FebProgClass.R
@@ -1,10 +1,60 @@
 
 # 12 Feb 2025
-########### ARRAYS ##############################
+#0000000000000000
 
-# Array like vectors, vectors are 2 dimenstional and array mor than 2 dimensions
 
+# A vector is the basic data type in R, contains list of items of the same type
+#Vectors
+############################
+# apple <- c("red", "blue", "yellow", 200, 30)
+# myvec <- c(1, 2, 3 , 4, 5)
+# 
+# try_vector <- c("aa", "bb", "cc", "dd", "ee")
+# print(length(try_vector))
+
+
+#Matrices
+############################
+# A matrix is 2-dimensional data of the same type. like table with rows and columns
+# m <- matrix(c("a", "a", "b", "c", "b", "a"), nrow = 2, ncol = 3, byrow = TRUE)
+# print(m)
+# 
+# m_example <- matrix(c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12), nrow = 3, ncol = 4, byrow = TRUE)
+# print(m_example)
+# 
+# # you can replace the vector 1 to 12 with seq(1,12)
+# m2 <- matrix(c(seq(5, 60, 5)), nrow = 3, ncol = 4, byrow = TRUE)
+# print(m2)
+# 
+# # 3 by 5 matrix starting from 20 to 6, with multiples 
+# # SHE USED BYROW FALSE AND IT STILL WORKED, ASK PROF
+# m3 <- matrix(c(seq(20, 6)), 3, 5, byrow = TRUE)
+# print(m3)
+
+
+# Lists
+############################
+# A list is an ordered collection of objects, like vectors, 
+# but lists can actually combine objects of different types. 
+
+# list1 <- list(c(2, 5, 3), 21.3, "sin")
+# print(list1)
+# 
+# thislist <- list("apple", "banana", "cherry")
+# thislist[1]
+# thislist[2]
+
+
+# 12 Feb 2025
+#0000000000000000
+
+# Arrays
+############################
+
+# Array like vectors, vectors are 2 dimensional and array more than 2 dimensions
+# Note: Arrays can only have one data type.
 # dim = c(num_rows, num_cols, num_arrays)
+
 # a <- array(c('green', 'yellow'), dim = c(3, 3, 2))
 # print(a)
 # 
@@ -13,41 +63,128 @@
 # print(b)
 
 
-# NB: READ ON VECTOR, ARRAY, LIST, MATRIX, FACTOR, AND ALL DATA TYPES IN R PROGRAMMING
 
-
-
-########### Factors ################# 
-
-# First create a vector
+# Factor
+############################
+# Factors are used to categorize data. Examples of factors are:
+  #Demography: Male/Female
+  #Music: Rock, Pop, Classic, Jazz
+  #Training: Strength, Stamina
+  #First create a vector
 
 # apple_colors <- c("green", "green", "yellow", "red", "red", "red", "green")
+# # 
+# # # now create a factor from the vector
+# factor_apple <- factor(apple_colors)
+# print(factor_apple)
+# print(nlevels(factor_apple))
+
+
+# Data frame
+############################
+# # Data Frames are data displayed in a format as a table.
+# # Data Frames can have different types of data inside it. 
+# # DATAFRAME - Just like vector but tabular and can contain different kinds of data types
 # 
-# # now create a factor from the vector
-factor_apple <- factor(apple_colors)
-print(factor_apple)
-print(nlevels(factor_apple))
-
-
-DATAFRAME - Just like vector but tabular and can contain different kinds of data types
-TABULAR DATA OBJECTS
-
-bmi <- data.frame(gender=c("male", "male", "female"),
-                  height=c(152, 171.5, 165),
-                  weight=c(81, 93, 78),
-                  age=c(42, 28, 36)
-                )
-
-
-
-bmi <- data.frame(gender=c("male", "male", "female", "male", "female", "male"),
-                  marital_status=c("single", "maried", "divorced", "separated", "co-habiting", "single"),
-                  height=c(152, 171.5, 165, 713, 174, 152),
-                  weight=c(81, 93, 78, 83, 79, 182),
-                  age=c(42, 28, 36, 28, 25, 33)
-                  )
-print(bmi)
-
+# bmi <- data.frame(gender=c("male", "male", "female"),
+#                   height=c(152, 171.5, 165),
+#                   weight=c(81, 93, 78),
+#                   age=c(42, 28, 36)
+#                 )
+# 
+# 
+# 
+# bmi <- data.frame(gender=c("male", "male", "female", "male", "female", "male"),
+#                   marital_status=c("single", "maried", "divorced", "separated", "co-habiting", "single"),
+#                   height=c(152, 171.5, 165, 713, 174, 152),
+#                   weight=c(81, 93, 78, 83, 79, 182),
+#                   age=c(42, 28, 36, 28, 25, 33)
+#                   )
+# print(bmi)
+# 
+# 
+# print(bmi[5,3])
+# print(bmi$height[5])
+# print(bmi[5,])
+# print(bmi[,3])
+# print(bmi$height)
 
 
 
+# 19TH FEB 2025
+#000000000000000000
+
+# Decisions (If conditions)
+############################
+
+# if (condition) {
+#   <code to run if condition is met>
+# }
+
+# n <- 9
+# if (a == 6) {
+#   print("Value in a is 6")
+# } else {
+#   print("Value is a is not 6")
+# }
+# 
+# 
+# a <- 10
+# 
+# if (a %% 2 == 0) {
+#   print("Number is even")
+# } else {
+#   print("Number is odd")
+# }
+# 
+# str1 <- "hello"
+# str2 <- "Hello"
+# if (str1 == str2) {
+#   print("The two strings are equal")
+# } else {
+#   print("The two strings are unequal")
+# }
+# 
+# 
+# n <- 14
+# if ((n %% 2 == 0) || (n %% 3 == 0)) {
+#   print("Number is even or divisible")
+# }
+# 
+# n <- 11
+# 
+# if (!(n %% 2 == 0)) {
+#   print("Number is not even")
+# } else {
+#   print("Number is odd")
+# }
+
+# a <- 15
+# 
+# if (a == 6) {
+#   print("a is 6")
+# } else if (a == 10) {
+#   print("a is 10")
+# } else if (a == 20) {
+#   print("a is 20")
+# } else {
+#   print("a is neither of the above")
+# }
+# 
+# print("End of program")
+
+
+
+# Switch Statement
+############################
+
+y <- 3
+
+x <- switch (
+  y,
+  "Good Morning",
+  "Good Afternoon",
+  "Good Evening",
+  "Good Night"
+)
+print(x)
