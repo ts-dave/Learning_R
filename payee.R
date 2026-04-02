ce55a38b29eadd5c99bc862d79f59d68eb8d6f9d
Tsatsu Dave <tsatsu.code@gmail.com>
2026-04-02 11:13:01 +0000

    Presented Payee code and made edits

--- payee.R
+++ payee.R
@@ -1,17 +1,17 @@
-
+# FUNCTION TO 
 payee_tax <- function(income) {
   total_tax <- 0
   
-  if (income <= 402) {     
+  if (income <= 402) {
     total_tax <- 0
     
-  } else if (income <= 512) {   
+  } else if (income <= 512) {
     total_tax <- (income - 402) * 0.05
     
-  } else if (income <= 642) {    
+  } else if (income <= 642) {
     total_tax <- 5.5 + (income - 512) * 0.10
     
-  } else if (income <= 3642) { 
+  } else if (income <= 3642) {
     total_tax <- 18.5 + (income - 642) * 0.175
     
   } else if (income <= 20037) {
@@ -23,27 +23,54 @@ payee_tax <- function(income) {
   } else {
     total_tax <- 13631.15 + (income - 50000) * 0.35
   }
-
+  
   return(total_tax)
 }
 
 
-print(payee_tax(5000))
+print(payee_tax(3000))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # TODO: Refactor the payee_tax function so that the values and tax rates 
-# can be dynamic, not hard coded in to the fucntion.
+# can be dynamic, not hard coded in to the function.
 
 
 # A FAME LOOP FOR CALLING PAYEE FUNCTION
-while (TRUE) {
-  income <- suppressWarnings(as.numeric(readline(prompt = "Please enter you salary amount: ")))
-  
-  if (income == 0) {
-    break
-  }
-  print(payee_tax(income))
-}
+# while (TRUE) {
+#   income <- suppressWarnings(as.numeric(readline(prompt = "Please enter you salary amount: ")))
+#   
+#   if (income == 0) {
+#     break
+#   }
+#   
+#   print(payee_tax(income))
+# }
 
 
 
