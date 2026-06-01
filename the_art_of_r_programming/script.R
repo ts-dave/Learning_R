6fbe3be1552381cadd34e30d967dcede2b6c8f47
Tsatsu Dave <tsatsu.code@gmail.com>
2026-06-01 04:09:13 +0000

    working with vectors

--- the_art_of_r_programming/script.R
+++ the_art_of_r_programming/script.R
@@ -13,13 +13,13 @@ length(x)
 # USE NEGATIVE INDEX IN VECTORS TO EXCLUDE ELEMENT ELEMENT 
 # AT THAT INDEX FROM THE NEW VECTOR
 y <- c(88, 5, 12, 13)
-y <- y[-1] #REMOVES FIRST ELEMENT FROM VECTOR
+y <- y[-1] #All elements except the first
 print(y)
-y <- y[-length(y)] #REMOVES LAST ELEMENT FROM RESULTING VECTOR
+y <- y[-length(y)] #All elements except the last
 print(y)
 
 
-## REPEATINB VECTORS USING rep()
+## REPEATINB VECTORS USING rep() function
 # rep(x, times)
 new <- c(5,12,13)
 rep(new,3)
@@ -39,3 +39,99 @@ any(x > 8) # TRUE
 any(x > 88) # FALSE
 all(x > 88) # FALSE
 all(x > 0) # TRUE
+
+
+## PREDICTING WEATHER
+
+preda <- function(x,k) {
+  n <- lenght(x)
+  k2 <- k/2
+  
+  # the vector pred will contain our predicted values
+  pred <- vector(length-n-k)
+  for (i in 1:(n-k)) {
+    if (sum(x[i:(k-1)]) >= k2) {
+      pred[i] <- 1
+    } else {
+      pred[i] <- 0
+    }
+  }
+  return(mean(abs(pred-x[(k+1):n])))
+  
+}
+
+
+
+### VECTORIZED OPERATIONS ###
+
+# Adding one to every member of the vector
+u <- c(5,2,8)
+
+w <- function(x) {
+  return(x+1)
+}
+
+w(u)
+
+# Rounding every member of the vector
+y <- c(1.2, 3.9, 0.4)
+z <- round(y)
+z
+
+
+### VECTOR IN MATRIX OUT ###
+
+z12 <- function(z) {
+  return(c(z, z^2))
+}
+
+x <- 1:8
+z12(x)
+
+#rewriting output as matrix
+matrix(z12(x), ncol = 2)
+
+
+# HANDS-ON PRACTICE
+# Trying my hands on vector in, matrix out
+
+# function return number and 2xnumber in vector form
+dup <- function(y) {
+  return(c(y, y * 2))
+}
+
+# calling function
+y <- 1:10
+dup(y)
+
+matrix(dup(y), ncol = 2) #coercing output into matrix format
+
+sapply(y, dup)
+
+
+
+### NA AND NULL VALUES ###
+
+# NA
+x <- c(88, NA, 12, 168, 13)
+x
+mean(x)
+mean(x, na.rm = TRUE) # remove NA with "na.rm = TRUE"
+
+# NULL
+z <- NULL
+
+for (i in 1:10) {
+  if (i %% 2 == 0) {
+    z <- c(z, i)
+  }
+}
+
+z
+
+# NB: NULL is a special R object with no code.
+
+
+
+### FILTERING ###
+# 
--- thegraphcourses/fossa.R
+++ thegraphcourses/fossa.R
@@ -11,3 +11,28 @@
 whitehall.data <- read.csv("data/Whitehall_fossa.csv", header=TRUE, na.strings = c(""))
 
 nrow(whitehall.data)
+ncol(whitehall.data)
+
+class(whitehall.data)
+class(whitehall.data$frailty)
+class(whitehall.data$currsmoker)
+unique(whitehall.data$currsmoker)
+median(whitehall.data$sbp)
+
+str(whitehall.data)
+head(whitehall.data, n=5)
+tail(whitehall.data, n=5)
+summary(whitehall.data)
+
+summary(whitehall.data$sbp)
+
+summary(whitehall.data$bmi_grp4)
+
+str(whitehall.data$bmi_grp4)
+summary(whitehall.data$bmi_grp4)
+
+whitehall.data$currsmoker <- factor(whitehall.data$currsmoker, labels = c("No", "Yes"))
+
+
+
+
