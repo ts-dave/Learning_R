0cb171178436d0168d6d4dd3a47483721331ebd6
Tsatsu Dave <tsatsu.code@gmail.com>
2026-08-06 16:26:42 +0000

    Finished with mutate() and select()

--- r_for_data_science/chapter_4_data_transformation.R
+++ r_for_data_science/chapter_4_data_transformation.R
@@ -48,5 +48,59 @@ flights |>
   count(origin, dest, sort = TRUE)
 
 
+
 ##################################################################
 #### COLUMNS ####
+# There are four important verbs that affect the columns without changing the rows: 
+# mutate() creates new columns that are derived from the existing columns, 
+# select() changes which columns are present, 
+# rename() changes the names of the columns, and 
+# relocate() changes the positions of the columns.
+
+
+#### MUTATE(): ####
+### mutate() adds new columns that are calculated from the existing columns.
+
+flights |> 
+  mutate( 
+    gain = dep_delay - arr_delay,
+    speed = distance / (air_time / 60),
+    .before = 1 # .before and/or .after to rearrange mutated coloumns.
+  )
+
+flights |> 
+  mutate(
+    gain = dep_delay - arr_delay,
+    speed = distance / (air_time / 60),
+    .after = day #columns names can be used instead of numbers
+  )
+
+# Use .keep = "used" to keep only columns used in the mutate function
+delay_gain <- flights |> 
+  mutate(
+    gain = dep_delay - arr_delay,
+    hours = air_time / 60,
+    gain_per_hour = gain / hours,
+    .keep = "used"
+  )
+
+#### SELECT(): ####
+### select() selects certain colouns in your dataset to zoom in on.
+flights |> 
+  select(year, month, day)
+
+flights |> 
+  select(year:day) #select columns from year to day
+
+flights |> 
+  select(!year:day) #all colomns except from year to day
+
+flights |> 
+  select(where(is.character)) #select all columns that are characters
+
+# Read about other helpers (everything(), starts_with(), ends_with(), contains(), etc.)
+
+
+#### RNAME(): ####
+### rename() used to change coloumns names
+# .
